d8c0be62c6360866e1a05a011064b71f3c258ef8
PageFoundry <admin@pagefoundry.de>
2025-10-23 12:29:25 +0200

    Update and rename account.ts to account.component.ts
    Angular fehler, wird nach component gesucht by default

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-account',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './account.html',
+  styleUrls: ['./account.css']
+})
+export class AccountComponent {
+  // Platzhalter-Logik oder Dashboard-Navigation kommt später hier rein
+}
