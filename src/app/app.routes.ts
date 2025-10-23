8669a917310e61108fda5c812c6d3eb54df5d917
PageFoundry <admin@pagefoundry.de>
2025-10-23 12:14:47 +0200

    Update app.routes.ts
    /account routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,22 @@ import { About } from './about/about';
 import { Projects } from './projects/projects';
 import { Contact } from './contact/contact';
 import { Login } from './login/login';
+import { AuthGuard } from './guards/auth.guard'; // <— wird gleich erstellt
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'contact', pathMatch: 'full' },
+  { path: '', redirectTo: 'contact', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'about', component: About },
   { path: 'projects', component: Projects },
   { path: 'contact', component: Contact },
   { path: 'login', component: Login },
+
+  // ✅ Lazy Loaded Account Page
+  {
+    path: 'account',
+    loadComponent: () => import('./account/account').then(m => m.Account),
+    canActivate: [AuthGuard]
+  },
+
   { path: '**', redirectTo: 'contact' }
 ];
